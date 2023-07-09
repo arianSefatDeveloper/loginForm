125db760fe153af3665c4b927888c9a289d56054
mmdreza <m13r81a@gmail.come>
2023-07-09 19:02:13 +0330

    password input finished

--- script.js
+++ script.js
@@ -6,15 +6,15 @@
 
 
 
- const leftEyeTop = 75;
- const leftEyeLeft = 110;
- const leftEyePaddingLeft = 0
- const leftEyePaddingTop = 0
+ let  leftEyeTop = 75;
+ let  leftEyeLeft = 110;
+ let  leftEyePaddingLeft = 0
+ let  leftEyePaddingTop = 0
 
- const rightEyeTop = 75;
- const rightEyeLeft = 174;
- const rightEyePaddingLeft = 0
- const rigthEyePaddingTop = 0
+ let  rightEyeTop = 75;
+ let  rightEyeLeft = 174;
+ let  rightEyePaddingLeft = 0
+ let  rigthEyePaddingTop = 0
 
 
 
@@ -30,10 +30,50 @@
  }
  
  const passInputFocus = ()=>{
+
     console.log("focus");
+    let eyePosition = setInterval(()=>{
+        if(leftEyeTop === 60){
+            clearInterval(eyePosition)
+        }
+
+        leftEye.style.top = leftEyeTop +'px'
+        leftEye.style.left = leftEyeLeft+'px'
+
+        rightEye.style.top = rightEyeTop +'px'
+        rightEye.style.left = rightEyeLeft +'px'
+
+
+        leftEyeTop--
+        leftEyeLeft++
+
+        rightEyeLeft--
+        rightEyeTop--
+
+    },10)
  }
  const passInputBlur = ()=>{
     console.log("blur");
+
+    let eyePosition = setInterval(()=>{
+        if(leftEyeTop === 75){
+            clearInterval(eyePosition)
+        }
+
+        leftEye.style.top = leftEyeTop +'px'
+        leftEye.style.left = leftEyeLeft+'px'
+
+        rightEye.style.top = rightEyeTop +'px'
+        rightEye.style.left = rightEyeLeft +'px'
+
+
+        leftEyeTop++
+        leftEyeLeft--
+
+        rightEyeLeft++
+        rightEyeTop++
+
+    },10)
  }
 
 
